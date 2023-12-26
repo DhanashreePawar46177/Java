796f7a94fd16ea1ec49979e94f6aecd86e90f860
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-12-26 18:41:12 +0530

    Datatypes

---
+++ Basic/Datatypes.java
@@ -0,0 +1,34 @@
+package Basic;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		byte BNo = -76;
+		short SNo = 6543;
+		int INo = 876654320;
+		long LNo = 765432189;
+		float FNo = 87.898778f;
+		double DNo = 54.7654323456786543978;
+		char Ch = 'D';
+		boolean Value = true;
+
+		System.out.println(BNo);
+		System.out.println(SNo);
+		System.out.println(INo);
+		System.out.println(LNo);
+		System.out.println(FNo);
+		System.out.println(DNo);
+		System.out.println(Ch);
+		System.out.println(Value);
+
+		System.out.println("\n============================================\n");
+
+		System.out.println("BNo is a byte type of variable = " + BNo);
+		System.out.println("SNo is a short type of variable = " + SNo);
+		System.out.println("INo is a int type of variable = " + INo);
+		System.out.println("LNo is a Long type of variable = " + LNo);
+		System.out.println("FNo is a float type of variable = " + FNo);
+		System.out.println("DNo is a double type of variable = " + DNo);
+		System.out.println("Ch is a charcter type of variable = " + Ch);
+		System.out.println("Value is a boolean type of variable = " + Value);
+	}
+}
