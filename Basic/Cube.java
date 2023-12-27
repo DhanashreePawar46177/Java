4522f957e8932deb336a6be0684db98d20dacd01
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-12-27 10:31:45 +0530

    Cube

---
+++ Basic/Cube.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Cube {
+	public static void main(String[] args) {
+		int a = 5;
+		
+		System.out.println("Number = " +a);
+		//int cube = a*a*a;
+		
+		int cube = (int)Math.pow(5, 3);
+		
+		System.out.println(cube);
+	}
+}
