8747ea9f2e89615526e5e8a56ab211117b4493fb
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-12-28 11:02:53 +0530

    Swapping using third variable

---
+++ Basic/Swapping_Using_Third_Variable.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Swapping_Using_Third_Variable {
+	public static void main(String[] args) {
+		int Ans, No1 = 10, No2 = 20;
+		
+		Ans = No1;
+		No1 = No2;
+		No2 = Ans;
+		
+		System.out.println("No1 = " +No1);
+		System.out.println("No2 = " +No2);
+	}
+}
