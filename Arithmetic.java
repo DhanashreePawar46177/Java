f09566bea2de5c7745c0da9446459f236c18fab7
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-12-29 21:22:00 +0530

    Arithmetic Operators

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+package Operators;
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 34;
+		int c = a + b;
+		System.out.println("\n Addition is = " + c);
+
+		c = a - b;
+		System.out.println("\n Subtraction is = " + c);
+
+		c = a * b;
+		System.out.println("\n Multiplication is = " + c);
+
+		c = a / b;
+		System.out.println("\n Division is = " + c);
+
+		c = a % b;
+		System.out.println("\n Modulous is = " + c);
+	}
+}
