1e098b68d1fd9b08dd4e2246bb0d4486602bfd6f
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-01 21:17:00 +0530

    Logical Operator

---
+++ Operators/Logical.java
@@ -0,0 +1,36 @@
+package Operators;
+
+public class Logical {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+
+		boolean c;
+
+		System.out.println("\n a = " + a + " b = " + b);
+
+		c = a && a;
+		System.out.println("\n a && a = " + c);
+
+		c = a && b;
+		System.out.println("\n a && b = " + c);
+
+		c = b && a;
+		System.out.println("\n b && a = " + c);
+
+		c = b && b;
+		System.out.println("\n b && b = " + c);
+
+		c = a || a;
+		System.out.println("\n a || a = " + c);
+
+		c = a || b;
+		System.out.println("\n a || b = " + c);
+
+		c = b || a;
+		System.out.println("\n b || a = " + c);
+
+		c = b || b;
+		System.out.println("\n b || b = " + c);
+	}
+}
