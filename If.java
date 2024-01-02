ae3c4c6f146a19712ae9c92563567284c247b4a8
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-02 10:24:07 +0530

    If Statement

---
+++ If.java
@@ -0,0 +1,12 @@
+package Conditional_Statements;
+
+public class If {
+	public static void main(String[] args) {
+		int a = 50;
+
+		if(a > 10)
+		{
+			System.out.println("a is greater than 10");
+		}
+	}
+}
