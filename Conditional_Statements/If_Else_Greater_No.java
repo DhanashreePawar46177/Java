9edafa525e730cb23aa438ae30b6a4e2f1e2a034
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-03 08:39:06 +0530

    If Else Statement

---
+++ Conditional_Statements/If_Else_Greater_No.java
@@ -0,0 +1,17 @@
+package Conditional_Statements;
+
+public class If_Else_Greater_No {
+	public static void main(String[] args) {
+		int a = 50;
+
+		System.out.println("a = " +a);
+		if(a < 10)
+		{
+			System.out.println("a is greater than 10");
+		}
+		else
+		{
+			System.out.println("a is less than 10");
+		}
+	}
+}
