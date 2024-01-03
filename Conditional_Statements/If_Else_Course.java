cdfbd4ca72b63c151890786071626fec7415abc6
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-03 08:38:45 +0530

    If Else Statement

---
+++ Conditional_Statements/If_Else_Course.java
@@ -0,0 +1,17 @@
+package Conditional_Statements;
+
+public class If_Else_Course {
+	public static void main(String[] args) {
+		String Lang = "Java";
+
+		System.out.println("Language = " +Lang);
+		if(Lang == "C" || Lang == "CPP")
+		{
+			System.out.println("You will continue the course");
+		}
+		else 
+		{
+			System.out.println("You will not continue the course");
+		}
+	}
+}
