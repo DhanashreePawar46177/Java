e78db973a7ffd6be86e5e30ae0c1923f498b14fc
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-03 11:48:43 +0530

    Java

---
+++ Basic/First_Java_Program.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class First_Java_Program {
+	public static void main(String[] args) {
+		System.out.print("Hello World.");
+		System.out.println("India is My Country.");
+		System.out.println("88055");
+		System.out.println(88055);
+		System.out.println(11 + 22);
+
+		// Concatenation
+		System.out.println(91 + " is a telephonoc code of India");
+	}
+}
