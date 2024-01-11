561bd177c6b1dfa962798724616f00894207f54a
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-11 18:13:02 +0530

    for Loop

---
+++ for_India.java
@@ -0,0 +1,12 @@
+package Lopping_Statements;
+
+public class for_India {
+	public static void main(String[] args) {
+		String str = "India";
+		
+		for(int a = 1; a <= 10; a++)
+		{
+			System.out.println(str);
+		}
+	}
+}
