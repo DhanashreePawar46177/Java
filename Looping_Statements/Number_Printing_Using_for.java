4c35eb941075cbb9a1405dada3cb3fdbc5a0a71c
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-14 07:48:02 +0530

    for Loop

---
+++ Looping_Statements/Number_Printing_Using_for.java
@@ -0,0 +1,12 @@
+package Lopping_Statements;
+
+public class Number_Printing_Using_for {
+	public static void main(String[] args) {
+		
+		System.out.println("Numbers 1-10.");
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
