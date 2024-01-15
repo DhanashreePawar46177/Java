54c8cf52891ca15ed792c6d0f58637990a585fa8
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-01-15 11:17:41 +0530

    for loop

---
+++ Looping_Statements/Table_of_10_Using_for.java
@@ -0,0 +1,16 @@
+package Lopping_Statements;
+
+public class Table_of_10_Using_for {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.println("Table of 10");
+		
+		for(int i = 1; a <= 100; )
+		{
+			System.out.println(i + "." + a);
+			
+			a = a+10;
+		}
+	}
+}
