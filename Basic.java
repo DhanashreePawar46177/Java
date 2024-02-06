4a5065bec9e95f0b6b011f0d8d691b913aadb063
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-06 19:26:11 +0530

    String

---
+++ Basic.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Basic {
+	public static void main(String[] args) {
+		String s[] = {"Abc", "Pqr", "Xyz"};
+		Basic a1 = new Basic();
+		Basic a2 = new Basic();
+		Basic a3 = new Basic();
+		Basic Obj[] = {a1,a2,a3};
+	}
+}
