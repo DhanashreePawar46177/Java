611389733362024e8aa5ecffc67ed49732000783
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-08 20:44:27 +0530

    String

---
+++ String/Pick_My_Char.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Pick_My_Char {
+	public static void pickChar() {
+		String s = "Dhanashree";
+		
+		char a = s.charAt(2);
+		
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		pickChar();
+	}
+}
