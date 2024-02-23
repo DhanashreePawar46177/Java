4e17e2ba002b53ed1a14dbe9b138d272de42f8b2
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-23 20:15:30 +0530

    String

---
+++ String/Use_Of_String.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Use_Of_String {
+	static int a = 21;
+	static String s = "Dhanu";
+	
+	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println(s);
+	}
+}
