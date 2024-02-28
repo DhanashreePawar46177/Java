d89c46f82369a9e2675bb6583aae8df93bfe3c11
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-28 21:28:04 +0530

    Multi Class Communication

--- Addition_Using_Loop/Main_Class.java
+++
@@ -1,8 +0,0 @@
-package Multi_Class_Communication.Addition_Using_Loop;
-
-public class Main_Class {
-	public static void main(String[] args) {
-		Addition_Using_Loop a = new Addition_Using_Loop();
-		a.Addition();
-	}
-}
