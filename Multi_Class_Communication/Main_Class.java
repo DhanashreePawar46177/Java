2ce6f9e99873d8be66585ffda6859fdf270f09cc
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-29 18:50:36 +0530

    Main_Class

---
+++ Multi_Class_Communication/Main_Class.java
@@ -0,0 +1,17 @@
+package Multi_Class_Communication.Arithmetic_Operations;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Addition a = new Addition();
+		a.add(20, 30);
+		
+		Subtraction s = new Subtraction();
+		s.sub(20, 30);
+		
+		Multiplication m = new Multiplication();
+		m.mult(20, 30);
+		
+		Division d = new Division();
+		d.div(60, 30);
+	}
+}
