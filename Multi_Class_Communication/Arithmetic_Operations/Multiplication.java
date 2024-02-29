af541abe8a46faa1cfeca5f65ede0dd6fa0f9ce1
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-02-29 18:53:39 +0530

    Multi Class

---
+++ Multi_Class_Communication/Arithmetic_Operations/Multiplication.java
@@ -0,0 +1,7 @@
+package Multi_Class_Communication.Arithmetic_Operations;
+
+public class Multiplication {
+	public void mult(int a, int b) {
+		System.out.println("Multiplication = " +(a*b));
+	}
+}
---
+++ Multi_Class_Communication/Arithmetic_Operations/Subtraction.java
@@ -0,0 +1,7 @@
+package Multi_Class_Communication.Arithmetic_Operations;
+
+public class Subtraction {
+	public void sub(int a, int b) {
+		System.out.println("Subtraction = " +(a-b));
+	}
+}
