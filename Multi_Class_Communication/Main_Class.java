6c0af3704002d736c17b4308f696a9ec8aa8b7c7
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-03-03 21:33:02 +0530

    Main Class

---
+++ Multi_Class_Communication/Main_Class.java
@@ -0,0 +1,12 @@
+package Multi_Class_Communication.Palindrome_Armstrong_Prime;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Check_Palindrome_Armstrong a = new Check_Palindrome_Armstrong();
+		a.Check_Palindrome();
+		a.Armstrong();
+		
+		Check_Prime p = new Check_Prime();
+		p.Prime();
+	}
+}
