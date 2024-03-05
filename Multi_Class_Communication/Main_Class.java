6e18f4b0c679e8462af91cef33add2ae0f513a4f
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-03-05 18:20:56 +0530

    User Input Addition

---
+++ Multi_Class_Communication/Main_Class.java
@@ -0,0 +1,8 @@
+package Multi_Class_Communication.User_Input_Addition;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		User_Input_Add a = new User_Input_Add();
+		a.User_Input();
+	}
+}
