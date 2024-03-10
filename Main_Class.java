8fe6ca65f3f29ffac2ac207d6afa74482786d434
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-03-10 14:26:50 +0530

    Main

---
+++ Main_Class.java
@@ -0,0 +1,11 @@
+package Constructor.Arithmetic_Operations;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Arithmetic obj = new Arithmetic();
+		obj.Add();
+
+		Arithmetic obj1 = new Arithmetic(12,34);
+		obj1.Add();
+	}
+}
