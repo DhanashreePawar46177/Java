94c557a30af3db75fa4786c3a49ca0d26b0ce188
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-03-16 21:10:06 +0530

    Main_Class

---
+++ Constructor/Main_Class.java
@@ -0,0 +1,14 @@
+package Constructor.Non_Parameterized.Constructor_Calling;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			Class01 one = new Class01();
+			Class02 two = new Class02();
+			Class03 three = new Class03();
+			Class04 four = new Class04();
+			
+			System.out.println("___________________________");
+		}
+	}
+}
