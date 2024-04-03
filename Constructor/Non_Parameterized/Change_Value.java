ed888119979c90f74e5678987eee4a6bf3d5bbe6
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-03 13:35:53 +0530

    Change Value

---
+++ Constructor/Non_Parameterized/Change_Value.java
@@ -0,0 +1,13 @@
+package Constructor.Non_Parameterized.Using_for_Value_Initialize;
+
+public class Change_Value {
+	int a = 20;
+	int b = 40;
+	
+	Change_Value(){
+		a = 50;
+		b = 100;
+		
+		System.out.println("Constructor Change_Value");
+	}
+}
