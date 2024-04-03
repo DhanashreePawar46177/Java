3722c0cc4da56bfd486b9ffce3a11a5b18eba817
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-03 13:37:09 +0530

    Main Class

---
+++ Constructor/Non_Parameterized/Using_for_Value_Initialize/Main_Class.java
@@ -0,0 +1,15 @@
+package Constructor.Non_Parameterized.Using_for_Value_Initialize;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Change_Value obj = new Change_Value();
+		
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		
+		Change_Value obj1 = new Change_Value();
+		
+		System.out.println(obj1.a);
+		System.out.println(obj1.b);
+	}
+}
