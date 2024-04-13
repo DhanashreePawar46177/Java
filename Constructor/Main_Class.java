e5f05aa6d92a21801f4b2f9fbd4d2f32f35e21f5
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-13 21:25:29 +0530

    Constructor

---
+++ Constructor/Main_Class.java
@@ -0,0 +1,10 @@
+package Constructor.Parameterized.Change_Value;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Change_Value obj = new Change_Value(0,0);
+		
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+	}
+}
