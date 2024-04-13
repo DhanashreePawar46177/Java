3abcea826703867dbbc92bfefc89ca7472ab20c4
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-13 21:27:09 +0530

    Change Value Class

---
+++ Constructor/Paramerized/Change_Value/Change_Value.java
@@ -0,0 +1,12 @@
+package Constructor.Parameterized.Change_Value;
+
+public class Change_Value {
+	int a = 23;
+	int b = 25;
+	
+	Change_Value(int a, int b){
+		a = 1; 
+		b = 2;
+		System.out.println("Constructor Change_Value");
+	}
+}
