91347de8cf358073038a79d522122fd7ceec4abe
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-14 19:56:38 +0530

    Main Class

---
+++ Constructor/Paramerized/Main_Class.java
@@ -0,0 +1,10 @@
+package Constructor.Parameterized.Change_Value_By_Parameter;
+
+public class Main_Class {
+	public static void main(String[] args) {
+		Change_Value_By_Parameter obj = new Change_Value_By_Parameter(0,0);
+		
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+	}
+}
