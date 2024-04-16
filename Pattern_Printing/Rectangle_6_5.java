c600ca7778b30fe65a8a47d45d406418e8a8d66e
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-04-16 19:01:26 +0530

    Pattern Printing

---
+++ Pattern_Printing/Rectangle_6_5.java
@@ -0,0 +1,12 @@
+package Pattern_Printing;
+
+public class Rectangle_6_5 {
+	public static void main(String[] args) {
+		for(int c = 1; c <= 5; c++) {
+			System.out.println();
+			for(int r = 1; r <= 5; r++) {
+				System.out.print(" * ");
+			}
+		}
+	}
+}
