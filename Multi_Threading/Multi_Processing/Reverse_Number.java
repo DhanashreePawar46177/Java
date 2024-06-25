987e7f9a7b0c60bd8b3baa4d0e6cde815eec7b24
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2024-06-25 18:13:14 +0530

    Delete Multi_Threading directory

--- Multi_Threading/Multi_Processing/Reverse_Number.java
+++
@@ -1,9 +0,0 @@
-package Multi_Threading.Multi_Processing;
-
-public class Reverse_Number implements Runnable{
-	public void run() {
-		for(int i = 10; i >= 1; i--) {
-			System.out.println("r " +i);
-		}
-	}
-}
--- Multi_Threading/Multi_Processing/Serial_Number.java
+++
@@ -1,9 +0,0 @@
-package Multi_Threading.Multi_Processing;
-
-public class Serial_Number extends Thread {
-	public void run() {
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("s " +i);
-		}
-	}
-}
